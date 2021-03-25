3063042c616d3dc1ebc064d053cf7f6ea75d3f04
elhadjiomar <simbadeveloppement@gmail.com>
2021-03-25 14:42:15 +0100

    initial commit

---
+++ src/main/java/uvs/sn/dscos/DscosApplication.java
@@ -0,0 +1,13 @@
+package uvs.sn.dscos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uvs/sn/dscos/DscosApplicationTests.java
@@ -0,0 +1,13 @@
+package uvs.sn.dscos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
